d8bec1428a000e1aa5b869251627f2d72ae1f638
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2026-02-17 18:54:53 +0200

    chore: created profile model

--- musicApp/profiles/models.py
+++ musicApp/profiles/models.py
@@ -1,3 +1,22 @@
+from django.core.validators import MinLengthValidator, RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    username = models.CharField(
+        max_length=15,
+        validators=[
+            MinLengthValidator(2),
+            RegexValidator(
+                regex=r'^[A-z0-9_]+$',
+                message='Ensure this value contains only letters, numbers, and underscore.',
+            )
+        ]
+    )
+
+    email = models.EmailField()
+
+    age = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+    )
