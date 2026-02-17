f889acfc5df358c103243ac2bcc8a10cb66d2112
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2026-02-17 20:05:54 +0200

    chore: created forms

---
+++ musicApp/albums/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from albums.models import Album
+
+
+class AlbumBaseForm(forms.ModelForm):
+    class Meta:
+        model = Album
+        fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Album Name'}),
+            'artist': forms.TextInput(attrs={'placeholder': 'Artist'}),
+            'genre': forms.TextInput(attrs={'placeholder': 'Genre'}),
+            'description': forms.Textarea(attrs={'placeholder': 'Description'}),
+            'image_url': forms.URLInput(attrs={'placeholder': 'Image URL'}),
+            'price': forms.NumberInput(attrs={'placeholder': 'Price'}),
+            'owner': forms.HiddenInput(),
+        }
+
+
+class AlbumCreateForm(AlbumBaseForm):
+    ...
+
+
+class AlbumEditForm(AlbumBaseForm):
+    ...
+
+
+class AlbumDeleteForm(AlbumBaseForm):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['readonly'] = True
+            field.widget.attrs['disabled'] = True
